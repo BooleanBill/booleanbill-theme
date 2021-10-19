b8e311b282d2a20cc4b6c606117add00123ac837
Martin Klejewski <m.klejewski@ovan.de>
2021-10-19 09:33:19 +0200

    crt save

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,23 @@
-const crt = document.querySelector('#crt_switch')
+const crt = document.querySelector('#crt_switch');
 crt.addEventListener('click', () => {
 	if (document.body.classList.contains('crt')) {
-		crt.classList.add('off')
-		crt.classList.remove('on')
-		document.body.classList.remove('crt')
+		crt.classList.add('off');
+		crt.classList.remove('on');
+		document.body.classList.remove('crt');
+		localStorage.setItem('crt', 'off');
 	} else {
-		document.body.classList.add('crt')
-		crt.classList.remove('off')
-		crt.classList.add('on')
+		document.body.classList.add('crt');
+		crt.classList.remove('off');
+		crt.classList.add('on');
+		localStorage.setItem('crt', 'on');
 	}
-})
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	let crtChoice = localStorage.getItem('crt');
+	if (crtChoice === 'off' && crtChoice !== null) {
+		document.body.classList.remove('crt');
+		crt.classList.add('off');
+		crt.classList.remove('on');
+	}
+});
